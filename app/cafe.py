79e0d0014b3b406ae71f18bed54f848f5191d7db
Khrystyna <tkachristina079@gmail.com>
2026-09-04 23:25:46 +0300

    'Solutio'

--- app/cafe.py
+++ app/cafe.py
@@ -13,17 +13,17 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
-                f"{visitor["name"]} is not vaccinated"
+                f'{visitor["name"]} is not vaccinated'
             )
         expiration_date = visitor["vaccine"]["expiration_date"]
 
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError(
-                f"{visitor["name"]} has an outdated vaccine"
+                f'{visitor["name"]} has an outdated vaccine'
             )
         if visitor["wearing_a_mask"] is False:
             raise NotWearingMaskError(
-                f"{visitor["name"]} is not wearing a mask"
+                f'{visitor["name"]} is not wearing a mask'
             )
 
         return f"Welcome to {self.name}"
